ddff9d501c00fe0c1fa319841529c4d652b83ea7
ssorina <sorina.negrila@3pillarglobal.com>
2015-01-07 17:09:35 +0200

    added homepage functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,6 +12,12 @@ class HomePageTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_titles_are_on_page(self):
+        self.browser.get('http://127.0.0.1:8000/home/')
+        title = self.browser.find_elements_by_tag_name('h1')
+        self.assertEqual(title[0].text, 'Mental health issues explained:')
+        self.assertEqual(title[1].text, 'Our registered psychologists:')
+
     def test_menu_element_on_homepage(self):
         self.browser.get('http://127.0.0.1:8000/home/')
         menu = self.browser.find_element_by_class_name('menu').text
@@ -28,7 +34,6 @@ class HomePageTest(unittest.TestCase):
         sender = self.browser.find_element_by_name('sender')
         sender.send_keys('test@test.com')
 
-        self.assertEqual(subject, 'Post')
 
 if __name__ == "__main__":
     unittest.main()
