2fb88e8d0dc42d8a100be3a00c12137ef5aff0b1
ssorina <sorina.negrila@3pillarglobal.com>
2015-01-07 17:01:32 +0200

    trying to add more tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,14 +8,27 @@ class HomePageTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.get('http://127.0.0.1:8000/home/')
 
     def tearDown(self):
-        self.browser = webdriver.Firefox()
         self.browser.quit()
 
-    def test_page_contains_both_h1(self):
-        self.browser = webdriver.Firefox()
-        menu = self.browser.find_elements_by_class_name('menu')
+    def test_menu_element_on_homepage(self):
+        self.browser.get('http://127.0.0.1:8000/home/')
+        menu = self.browser.find_element_by_class_name('menu').text
+        self.assertIn('Home', menu)
 
-        assert 'Home' in menu
+    def test_message_form_on_page(self):
+        self.browser.get('http://127.0.0.1:8000/home/')
+        subject = self.browser.find_element_by_name('subject')
+        subject.send_keys('Post')
+
+        message = self.browser.find_element_by_name('message')
+        message.send_keys('My message is good')
+
+        sender = self.browser.find_element_by_name('sender')
+        sender.send_keys('test@test.com')
+
+        self.assertEqual(subject, 'Post')
+
+if __name__ == "__main__":
+    unittest.main()
